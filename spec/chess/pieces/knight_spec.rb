69106e000ba726c8985a828e64f5f71ce2692e07
X_AJ_X <88792057+XAJX179@users.noreply.github.com>
2025-07-30 00:05:45 +0530

    test(spec): add and remove knight and pawn tests

--- spec/chess/pieces/knight_spec.rb
+++ spec/chess/pieces/knight_spec.rb
@@ -16,10 +16,39 @@ describe Chess::Pieces::Knight do
     end
 
     context 'when called on white Knight at a4 with black current player' do
-      it 'returns correct moves' do
+      it 'returns empty' do
         black_player_board = Chess::Board.new('2nkN3/P4P2/7n/n1pN3N/N3nP2/8/8/1p1KNnp1 b - - 0 1')
         piece = black_player_board.piece_at('a', 4)
         expect(piece.possible_moves(black_player_board)).to be_empty
+      end
+    end
+
+    context 'when called on white Knight at e1 with white current player' do
+      it 'returns correct moves' do
+        piece = board.piece_at('e', 1)
+        expect(piece.possible_moves(board)).to contain_exactly(['d', 3], ['f', 3], ['c', 2], ['g', 2])
+      end
+    end
+
+    context 'when called on white Knight at h5 with white current player' do
+      it 'returns correct moves' do
+        piece = board.piece_at('h', 5)
+        expect(piece.possible_moves(board)).to contain_exactly(['g', 7], ['g', 3], ['f', 6])
+      end
+    end
+
+    context 'when called on white Knight at d5 with white current player' do
+      it 'returns correct moves' do
+        piece = board.piece_at('d', 5)
+        expect(piece.possible_moves(board)).to contain_exactly(['c', 7], ['e', 7], ['c', 3], ['e', 3], ['b', 6],
+                                                               ['b', 4], ['f', 6])
+      end
+    end
+
+    context 'when called on white Knight at e8 with white current player' do
+      it 'returns correct moves' do
+        piece = board.piece_at('e', 8)
+        expect(piece.possible_moves(board)).to contain_exactly(['d', 6], ['f', 6], ['c', 7], ['g', 7])
       end
     end
   end
--- spec/chess/pieces/pawn_spec.rb
+++ spec/chess/pieces/pawn_spec.rb
@@ -8,117 +8,69 @@ describe Chess::Pieces::Pawn do
       display_board(board)
     end
 
-    # white pieces
-    context 'when called on a2' do
+    context 'when called on white Pawn at a2 with white current player' do
       it 'returns correct moves' do
         piece = board.piece_at('a', 2)
         expect(piece.possible_moves(board)).to contain_exactly(['a', 3], ['a', 4], ['b', 3])
       end
     end
 
-    context 'when called on b2' do
+    context 'when called on white Pawn at a4 with black current player' do
+      it 'returns empty' do
+        black_player_board = Chess::Board.new('3k4/ppp1pppp/1P2P1P1/4P3/4p3/1p2p1p1/PPP1PPPP/3K4 b - - 0 1')
+        piece = black_player_board.piece_at('a', 2)
+        expect(piece.possible_moves(black_player_board)).to be_empty
+      end
+    end
+
+    # white pieces
+
+    context 'when called on b2 with white current player' do
       it 'returns correct moves' do
         piece = board.piece_at('b', 2)
         expect(piece.possible_moves(board)).to be_empty
       end
     end
 
-    context 'when called on e4' do
+    context 'when called on e4 with white current player' do
       it 'returns correct moves' do
         piece = board.piece_at('e', 4)
         expect(piece.possible_moves(board)).to be_empty
       end
     end
 
-    context 'when called on c2' do
+    context 'when called on c2 with white current player' do
       it 'returns correct moves' do
         piece = board.piece_at('c', 2)
         expect(piece.possible_moves(board)).to contain_exactly(['c', 3], ['c', 4], ['b', 3])
       end
     end
 
-    context 'when called on f2' do
+    context 'when called on f2 with white current player' do
       it 'returns correct moves' do
         piece = board.piece_at('f', 2)
         expect(piece.possible_moves(board)).to contain_exactly(['f', 3], ['f', 4], ['e', 3], ['g', 3])
       end
     end
 
-    context 'when called on h2' do
+    context 'when called on h2 with white current player' do
       it 'returns correct moves' do
         piece = board.piece_at('h', 2)
         expect(piece.possible_moves(board)).to contain_exactly(['h', 3], ['h', 4], ['g', 3])
       end
     end
 
-    context 'when called on b6' do
+    context 'when called on b6 with white current player' do
       it 'returns correct moves' do
         piece = board.piece_at('b', 6)
         expect(piece.possible_moves(board)).to contain_exactly(['a', 7], ['c', 7])
       end
     end
 
-    context 'when called on e6' do
+    context 'when called on e6 with white current player' do
       it 'returns correct moves' do
         piece = board.piece_at('e', 6)
         expect(piece.possible_moves(board)).to contain_exactly(['f', 7])
-      end
-    end
-
-    # black pieces
-    context 'when called on a7' do
-      it 'returns correct moves' do
-        piece = board.piece_at('a', 7)
-        expect(piece.possible_moves(board)).to contain_exactly(['a', 6], ['a', 5], ['b', 6])
-      end
-    end
-
-    context 'when called on b7' do
-      it 'returns correct moves' do
-        piece = board.piece_at('b', 7)
-        expect(piece.possible_moves(board)).to be_empty
-      end
-    end
-
-    context 'when called on e5' do
-      it 'returns correct moves' do
-        piece = board.piece_at('e', 5)
-        expect(piece.possible_moves(board)).to be_empty
-      end
-    end
-
-    context 'when called on c7' do
-      it 'returns correct moves' do
-        piece = board.piece_at('c', 7)
-        expect(piece.possible_moves(board)).to contain_exactly(['c', 6], ['c', 5], ['b', 6])
-      end
-    end
-
-    context 'when called on f7' do
-      it 'returns correct moves' do
-        piece = board.piece_at('f', 7)
-        expect(piece.possible_moves(board)).to contain_exactly(['f', 6], ['f', 5], ['e', 6], ['g', 6])
-      end
-    end
-
-    context 'when called on h7' do
-      it 'returns correct moves' do
-        piece = board.piece_at('h', 7)
-        expect(piece.possible_moves(board)).to contain_exactly(['h', 6], ['h', 5], ['g', 6])
-      end
-    end
-
-    context 'when called on b3' do
-      it 'returns correct moves' do
-        piece = board.piece_at('b', 3)
-        expect(piece.possible_moves(board)).to contain_exactly(['a', 2], ['c', 2])
-      end
-    end
-
-    context 'when called on e3' do
-      it 'returns correct moves' do
-        piece = board.piece_at('e', 3)
-        expect(piece.possible_moves(board)).to contain_exactly(['f', 2])
       end
     end
   end
